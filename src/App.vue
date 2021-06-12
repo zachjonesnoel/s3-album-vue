00364bb7834c3e8bb6d899dfb1ea5b76ccc952c5
zachjonesnoel <joneszachariahnoel@gmail.com>
2021-06-12 13:56:37 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <amplify-authenticator>
+      <div v-if="authState === 'signedin' && user">
+        <div>Hello, {{user.attributes.email}}</div>
+        <amplify-sign-out @click="signOut()"></amplify-sign-out>
+      </div>
+      <S3browser/>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import S3browser from './components/s3browser.vue'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+import { Auth } from 'aws-amplify';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    S3browser
+  },
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined
+    }
+  },
+  beforeDestroy() {
+    this.unsubscribeAuth();
+  },
+  methods:{
+    async signOut() {
+    try {
+        await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
+}
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import "@aws-amplify/ui-vue";
+Amplify.configure(awsconfig);
 Vue.config.productionTip = false
 
 new Vue({
